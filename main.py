c4a7278108d0f6b4a08d0b2b99f517921346f3d7
Blaga Mihai <mihaiandrei_99@yahoo.com>
2021-02-25 16:19:20 +0200

    trask 2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This is a sample Python script.
 from task1 import run as run_task1
-from task2 import run as run_task2
+from task2 import run_addition as run_task2_addition
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 precision = run_task1()
-run_task2(precision)
+run_task2_addition()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- task2.py
+++ task2.py
@@ -1,5 +1,12 @@
-def run(precision):
-    a = 1.0
-    b = precision/10
-    c = precision/10
-    print((a+b)+c == a + (b+c))
+def run_addition():
+    print("task 2")
+    u = 1
+    not_equal = True
+    while not_equal:
+        pre = pow(10, -u)
+        a = 1.0
+        b = pre / 10
+        c = pre / 10
+        not_equal = bool((a + b) + c == a + (b + c))
+        u = u + 1
+    print("Precizia masina la asociativitatea adunatii: ", u)
